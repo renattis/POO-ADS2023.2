3b6a2f39af8b7530ef3ed2c44b5d2c6447fe1221
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2023-08-27 19:59:48 -0300

    Update Atividade4-Q1.js

--- Atividade4-Q1.js
+++ Atividade4-Q1.js
@@ -1,15 +1,1 @@
-//Crie uma função que recebe como parâmetro um número e retorna true se o número for par e false se for ímpar.
 
-def eh_par(numero):
-    if numero % 2 == 0:
-        return True
-    else:
-        return False
-
-numero = int(input("Digite um número: "))
-resultado = eh_par(numero)
-
-if resultado:
-    print("O número é par.")
-else:
-    print("O número é ímpar.")
