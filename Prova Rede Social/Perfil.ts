4200260a1582a4ac59460dfdd4c93ec4dd230f5a
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2023-11-12 19:49:54 -0300

    Update Perfil.ts

--- Prova Rede Social/Perfil.ts
+++ Prova Rede Social/Perfil.ts
@@ -1,16 +1,16 @@
+import Postagem from './Postagem'; 
+
 class Perfil {
   private _id: number;
   private _nome: string;
   private _email: string;
-  private _fileStorage: PerfilFileStorage;
   private _postagens: Postagem[]; 
 
-  constructor(id: number, nome: string, email: string, nomeArquivo: string) {
+  constructor(id: number, nome: string, email: string) {
     this._id = id;
     this._nome = nome;
     this._email = email;
-    this._fileStorage = new PerfilFileStorage(nomeArquivo);
-    this._postagens = []; 
+    this._postagens = [];
   }
 
   getId(): number {
@@ -25,42 +25,13 @@ class Perfil {
     return this._email;
   }
 
-  getPostagens(): Postagem[] {
+  getPostagens(): Postagem[] { // Adicionando o método
     return this._postagens;
   }
 
   adicionarPostagem(postagem: Postagem): void {
     this._postagens.push(postagem);
   }
-
-  salvarPerfil(): void {
-    const perfis = this._fileStorage.carregarPerfis();
-    const index = perfis.findIndex(perfil => perfil.getId() === this._id);
-
-    if (index !== -1) {
-      perfis[index] = this;
-    } else {
-      perfis.push(this);
-    }
-
-    this._fileStorage.salvarPerfis(perfis);
-  }
-
-  static consultarPerfil(id: number, nome: string, email: string, nomeArquivo: string): Perfil | null {
-    const perfis = new PerfilFileStorage(nomeArquivo).carregarPerfis();
-
-    if (id) {
-      return perfis.find(perfil => perfil.getId() === id) || null;
-    }
-
-    if (nome) {
-      return perfis.find(perfil => perfil.getNome() === nome) || null;
-    }
-
-    if (email) {
-      return perfis.find(perfil => perfil.getEmail() === email) || null;
-    }
-
-    return null;
-  }
 }
+
+export default Perfil;
