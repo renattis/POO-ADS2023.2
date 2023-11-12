affc64156599374b596df0f461e54ff733fe7f51
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2023-11-12 15:04:32 -0300

    Create Postagemavançada.ts

---
+++ Prova Rede Social/Postagemavançada.ts
@@ -0,0 +1,28 @@
+class PostagemAvancada extends Postagem {
+  private _hashtags: string[];
+  private _visualizacoesRestantes: number;
+
+  constructor(
+    id: number,
+    texto: string,
+    curtidas: number,
+    descurtidas: number,
+    data: Date,
+    perfil: Perfil,
+    hashtags: string[],
+    visualizacoesRestantes: number
+  ) {
+    super(id, texto, curtidas, descurtidas, data, perfil);
+    this._hashtags = hashtags;
+    this._visualizacoesRestantes = visualizacoesRestantes;
+  }
+
+  // Métodos get
+  getHashtags(): string[] {
+    return this._hashtags;
+  }
+
+  getVisualizacoesRestantes(): number {
+    return this._visualizacoesRestantes;
+  }
+}
