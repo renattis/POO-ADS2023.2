55f0763d287e97c8c1b1c8201c63069413a54d33
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2023-11-12 14:54:58 -0300

    Create Perfil.ts

---
+++ Prova Rede Social/Perfil.ts
@@ -0,0 +1,28 @@
+class Perfil {
+  private _id: number;
+  private _nome: string;
+  private _email: string;
+  private _postagens: Postagem[];
+
+  constructor(id: number, nome: string, email: string) {
+    this._id = id;
+    this._nome = nome;
+    this._email = email;
+    this._postagens = [];
+  }
+  getId(): number {
+    return this._id;
+  }
+
+  getNome(): string {
+    return this._nome;
+  }
+
+  getEmail(): string {
+    return this._email;
+  }
+
+  getPostagens(): Postagem[] {
+    return this._postagens;
+  }
+}
