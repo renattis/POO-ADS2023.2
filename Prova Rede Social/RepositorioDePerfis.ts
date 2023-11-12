28799870132b11bb145d42b8586e6433b4c24872
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2023-11-12 19:50:26 -0300

    Update RepositorioDePerfis.ts

--- Prova Rede Social/RepositorioDePerfis.ts
+++ Prova Rede Social/RepositorioDePerfis.ts
@@ -1,36 +1,53 @@
+import fs from 'fs';
+import Perfil from './Perfil';
+
 class RepositorioDePerfis {
   private _perfis: Perfil[];
+  private _nomeArquivo: string;
 
-  constructor() {
+  constructor(nomeArquivo: string) {
     this._perfis = [];
+    this._nomeArquivo = nomeArquivo;
+
+    // Carrega perfis do arquivo ao inicializar
+    this._perfis = this.carregarPerfis();
   }
 
   incluir(perfil: Perfil): void {
     this._perfis.push(perfil);
+    this.salvarPerfis(); // Salva os perfis após incluir no repositório
   }
 
   consultar(id?: number, nome?: string, email?: string): Perfil | null {
     if (id !== undefined) {
-      const perfilPorId = this._perfis.find((perfil) => perfil.getId() === id);
-      if (perfilPorId) {
-        return perfilPorId;
-      }
+      return this._perfis.find((perfil) => perfil.getId() === id) || null;
     }
 
     if (nome !== undefined) {
-      const perfilPorNome = this._perfis.find((perfil) => perfil.getNome() === nome);
-      if (perfilPorNome) {
-        return perfilPorNome;
-      }
+      return this._perfis.find((perfil) => perfil.getNome() === nome) || null;
     }
 
     if (email !== undefined) {
-      const perfilPorEmail = this._perfis.find((perfil) => perfil.getEmail() === email);
-      if (perfilPorEmail) {
-        return perfilPorEmail;
-      }
+      return this._perfis.find((perfil) => perfil.getEmail() === email) || null;
     }
 
     return null;
   }
+
+  private salvarPerfis(): void {
+    const perfisJSON = JSON.stringify(this._perfis, null, 2);
+    fs.writeFileSync(this._nomeArquivo, perfisJSON);
+  }
+
+  private carregarPerfis(): Perfil[] {
+    try {
+      const perfisJSON = fs.readFileSync(this._nomeArquivo, 'utf8');
+      return JSON.parse(perfisJSON) || [];
+    } catch (error) {
+      console.error('Erro ao carregar perfis do arquivo:', error.message);
+      return [];
+    }
+  }
 }
+
+export default RepositorioDePerfis;
